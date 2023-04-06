e9a00b6fed076cb9f7d3426327274fd0c17364c3
Konstantin Semenenko <mail@ksemenenko.com>
2023-04-06 13:12:36 +0200

    init

---
+++ ManagedCode.FeatureChecker.Tests/Tests.cs
@@ -0,0 +1,14 @@
+using ManagedCode.FeatureChecker;
+using Xunit;
+
+namespace ManagedCode.FeatureChecker.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void TestDetectorTest()
+    {
+
+    }
+    
+}
