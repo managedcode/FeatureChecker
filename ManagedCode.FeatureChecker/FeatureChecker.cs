255e9d992fbe7bc508af4542544e3ec84b54f662
Yaroslav-B <91521756+Yaroslav-B@users.noreply.github.com>
2023-04-20 18:59:31 +0300

    feat: implement 'Add..' methods and 'IsFeatureExists' method

--- ManagedCode.FeatureChecker/FeatureChecker.cs
+++ ManagedCode.FeatureChecker/FeatureChecker.cs
@@ -2,20 +2,58 @@
 
 public class FeatureChecker
 {
+    private readonly List<Feature> _features;
+
+    public FeatureChecker()
+    {
+        _features = new List<Feature>();
+    }
+
     public event EventHandler FeatureAdded;
     public event EventHandler FeatureRemoved;
     public event EventHandler FeatureStatusChanged;
 
-    public FeatureChecker() { }
+    public void AddFeature(string name, FeatureStatus status, bool ignoreDuplicates = true)
+    {
+        if(!IsFeatureExists(name))
+        {
+            var feature = new Feature()
+            {
+                Name = name,
+                Status = status
+            };
 
-    public void AddFeature(string name, FeatureStatus status) 
-        => throw new NotImplementedException();
+            _features.Add(feature);
+        }
 
-    public void AddFeature(Feature feature)
-        => throw new NotImplementedException();
+        if(!ignoreDuplicates)
+        {
+            throw new ArgumentException($"Feature '{nameof(name)}' is already added.");
+        }
+    }
 
-    public void AddFutureRange(IEnumerable<Feature> features)
-        => throw new NotImplementedException();
+    public void AddFeature(Feature feature, bool ignoreDuplicates = true)
+    {
+        if(feature == null)
+        {
+            throw new ArgumentNullException(nameof(feature), $"Parameter is null.");
+        }
+
+        AddFeature(feature.Name, feature.Status, ignoreDuplicates);
+    }
+
+    public void AddFeaturesRange(IEnumerable<Feature> features, bool ignoreDuplicates = true)
+    {
+        if(features == null)
+        {
+            throw new ArgumentNullException(nameof(features), $"Parameter is null.");
+        }
+
+        foreach(var feature in features)
+        {
+            AddFeature(feature, ignoreDuplicates);
+        }
+    }
 
     public void RemoveFeature(string name) 
         => throw new NotImplementedException();
@@ -29,8 +67,16 @@ public class FeatureChecker
     public void RemoveFeatureRange(IEnumerable<Feature> features)
         => throw new NotImplementedException();
 
-    public bool IsFeatureExists(string name) 
-        => throw new NotImplementedException();
+    public bool IsFeatureExists(string? name)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentNullException($"Invalid parameter '{nameof(name)}': {name}.");
+        }
+
+        return _features.Any(x =>
+            x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+    }
 
     public bool IsFeatureEnabled(string name)
         => throw new NotImplementedException();
