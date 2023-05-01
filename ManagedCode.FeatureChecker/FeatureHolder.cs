9590b5070045dce56e7d8660a871cb8a65d3e4e6
Yaroslav-B <91521756+Yaroslav-B@users.noreply.github.com>
2023-05-01 19:05:25 +0300

    feat: add FeatureHolder

---
+++ ManagedCode.FeatureChecker/FeatureHolder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+
+namespace ManagedCode.FeatureChecker;
+public class FeatureHolder
+{
+    public readonly Dictionary<string, FeatureStatus> _features;
+
+    public ImmutableDictionary<string, FeatureStatus> Features =>
+        _features.ToImmutableDictionary();
+
+    public FeatureHolder()
+    {
+        _features = new Dictionary<string, FeatureStatus>();
+    }
+}
