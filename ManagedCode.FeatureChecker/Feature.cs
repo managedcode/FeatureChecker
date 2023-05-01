d918f212c920ed437d114f749f18564531b78614
Yaroslav-B <91521756+Yaroslav-B@users.noreply.github.com>
2023-05-01 19:24:18 +0300

    feat: remove obsolete members of FeatureChecker and Feature model

--- ManagedCode.FeatureChecker/Feature.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ManagedCode.FeatureChecker;
-public class Feature
-{
-    public string Name { get; set; } = null!;
-    public FeatureStatus Status { get; set; }
-}
--- ManagedCode.FeatureChecker/FeatureChecker.cs
+++ ManagedCode.FeatureChecker/FeatureChecker.cs
@@ -1,47 +1,15 @@
-﻿namespace ManagedCode.FeatureChecker;
+﻿using System.Collections.Immutable;
+
+namespace ManagedCode.FeatureChecker;
 
 public class FeatureChecker
 {
-    private readonly Dictionary<string, Feature> _features;
+    private readonly ImmutableDictionary<string, FeatureStatus> _features;
 
-    public FeatureChecker()
+    public FeatureChecker(FeatureHolder featureHolder)
     {
-        _features = new Dictionary<string, Feature>();
+        _features = featureHolder.Features;
     }
-
-    public event EventHandler FeatureAdded;
-    public event EventHandler FeatureRemoved;
-    public event EventHandler FeatureStatusChanged;
-
-
-    public bool TryAddFeature(string name, FeatureStatus status)
-    {
-        var feature = new Feature()
-        {
-            Name = name,
-            Status = status
-        };
-
-        return TryAddFeature(feature);
-    }
-
-    public bool TryAddFeature(Feature newFeature)
-    {
-        ThrowIfNull(newFeature, nameof(newFeature));
-        ValidateModel(newFeature);
-
-        return _features.TryAdd(newFeature.Name, newFeature);
-    }
-
-
-    public void RemoveFeature(string name)
-    {
-        ThrowIfNullOrEmpty(name, nameof(name));
-        _features.Remove(name);
-    }
-
-    public void RemoveAllFeatures() => _features.Clear();
-
 
     public bool IsFeatureExists(string name)
     {
@@ -49,51 +17,7 @@ public class FeatureChecker
 
         return _features.ContainsKey(name);
     }
-
-    public bool TryGetFeatureStatus(string name, out FeatureStatus status)
-    {
-        var result = TryGetFeatureByName(name, out Feature? feature);
-        status = feature?.Status ?? default;
-
-        return result;
-    }
-
-    public bool TryGetFeatureByName(string name, out Feature? feature)
-    {
-        ThrowIfNullOrEmpty(name, nameof(name));
-
-        return _features.TryGetValue(name, out feature);
-    }
-
-
-    public Feature[] GetExistFeatures() => _features.Values.ToArray();
-
-    public IEnumerable<Feature> GetFeaturesByStatus(FeatureStatus status)
-    {
-        return _features.Values.Where(x => x.Status == status);
-    }
-
-
-    public bool TryUpdateFeatureStatus(string name, FeatureStatus status)
-    {
-        var result = TryGetFeatureByName(name, out Feature? feature);
-
-        if(result && feature != null)
-        {
-            feature.Status = FeatureStatus.Disabled;
-        }
-
-        return result;
-    }
-
-
-    private static void ThrowIfNull(object obj, string paramName)
-    {
-        if(obj == null)
-        {
-            throw new ArgumentNullException(paramName, $"Parameter is null.");
-        }
-    }
+    
 
     private static void ThrowIfNullOrEmpty(string arg, string argName)
     {
@@ -103,8 +27,4 @@ public class FeatureChecker
         }
     }
 
-    private static void ValidateModel(Feature feature)
-    {
-        //do some validation
-    }
 }
